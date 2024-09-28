58d7c09a313ae05309eb69fbb8626cd67bf8ba0e
root <root@DESKTOP-NJ5E2AI>
2024-09-28 18:51:25 -0500

    A'

--- back-end/code/src/App.java
+++ back-end/code/src/App.java
@@ -1,9 +1,9 @@
-import com.wrapper.spotify.*;
+import com.wrapper.spotify.SpotifyApi;
 public class App {
     public static void main(String[] args) throws Exception {
         
     }
     public void update(){
-        
+
     }
 }
--- back-end/code/src/Server.java
+++ back-end/code/src/Server.java
@@ -1,15 +1,21 @@
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 public class Server{
-    static ConcurrentLinkedQueue q;
+    static ConcurrentLinkedQueue<HashMap<String,Socket>> request;
+    static ConcurrentLinkedQueue<HashMap<List,Socket>> talking;
     public static void main(String[] args){
         try{
         String ip=InetAddress.getLocalHost().getHostAddress();
+        Thread talk=new Thread(new talkThread());
+        talk.run();
         ServerSocket server=new ServerSocket(443);
         while(true){
             Socket client=server.accept();
-
+            Thread listen=new Thread(new listenThread(client));
+            listen.run();
         }
     }
     catch(UnknownHostException e){
@@ -23,18 +29,37 @@ public class Server{
     }
     }
 }
-class acceptThread implements Runnable{
-    public void run(){
-
-    }
-}
 class listenThread implements Runnable{
+    Socket client;
+    BufferedReader br;
+    public listenThread(Socket client){
+        this.client=client;
+    }
     public void run(){
-
+        try{
+            while(true){
+                String s=br.readLine();
+                if(s.equals("song data")){
+                    HashMap<String,Socket> h=new HashMap<>();
+                    h.put("song data", client);
+                    Server.request.add(h);
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Fail");
+            System.out.println(e.toString());
+            return;
+        }
     }
 }
 class talkThread implements Runnable{
+    ObjectOutputStream pr;
     public void run(){
-
+        while(true){
+            while(!Server.talking.isEmpty()){
+                HashMap<List,Socket> o=Server.talking.poll();
+                
+            }
+        }
     }
 }
