1accc63e514f2e4aeb82761e596eeedb060d7f19
eodavis23 <144056500+eodavis23@users.noreply.github.com>
2024-09-28 18:46:17 -0500

    updated random song function to return an object

---
+++ front-end/game/game.js
@@ -0,0 +1,32 @@
+function playGame () {
+    //Startup Processes
+    /*
+    1. Get 2 songs (must have different popularity scores)
+    2. Update 
+    */
+   let gameOver = false
+   
+   let song1 = getSong(); //Get song 2
+   let song2 = getSong(); //Get song 2
+
+   while(gameOver == false) {
+    
+   }
+}
+
+function getSong () {
+
+}
+
+function updateSong (song) {
+    const albumCover = document.getElementById("leftAlbumCover");
+    const songTitle = '';
+    const songAuthor = '';
+
+    albumCover.setAttribute("src",song.cover);
+    
+}
+
+function updateImage2 () {
+    
+}
