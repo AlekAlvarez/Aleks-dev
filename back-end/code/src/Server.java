3374b562c016c91f3980500ab6c79ff8c738118e
root <root@DESKTOP-NJ5E2AI>
2024-09-28 16:20:02 -0500

    Begins serer code

--- back-end/code/src/Server.java
+++ back-end/code/src/Server.java
@@ -1,6 +1,26 @@
-import spotify.*;
+import java.io.*;
+import java.net.*;
 public class Server{
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnknownHostException{
+        String ip=InetAddress.getLocalHost().getHostAddress();
+        while(true){
+
+        }
         
+    }
+}
+class acceptThread implements Runnable{
+    public void run(){
+
+    }
+}
+class listenThread implements Runnable{
+    public void run(){
+
+    }
+}
+class talkThread implements Runnable{
+    public void run(){
+
     }
 }
