70e28e272c3e43a1d8fa73346af2541aa8c9cc2d
eodavis23 <144056500+eodavis23@users.noreply.github.com>
2024-09-29 06:51:17 -0500

    deleted unused code

--- app/tuturial.py
+++ app/tuturial.py
@@ -37,9 +37,7 @@ def login():
     auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
 
     return redirect(auth_url)
-@app.route("/loggedin")
-def loggedIn():
-    return loggedIn
+
 @app.route('/callback')
 def callback():
     if 'error' in request.args:
