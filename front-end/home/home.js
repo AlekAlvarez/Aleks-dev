9d54f34520cc7fbf9782cb94785829eb21b55697
Alex Flesher <alexflesher03@yahoo.com>
2024-09-29 00:18:18 -0500

    fixed button

--- front-end/home/home.js
+++ front-end/home/home.js
@@ -1,13 +1,8 @@
-import { getToken, getAuthHeader, searchForRandomSong } from "../../Spotify-API/api";
+// //import { getToken, getAuthHeader, searchForRandomSong } from "../../Spotify-API/api";
 
 document.addEventListener("DOMContentLoaded", function() {
     const startButton = document.getElementById("startButton"); // Use your button's ID
     startButton.addEventListener("click", function() {
         window.location.href = "../game/game.html"; // Navigate to the game page
-        startGame();
     });
-
-
-
-
 });
