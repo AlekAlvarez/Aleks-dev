698ff3f2628029e1c5fbba3a49620f6319f1447e
Alex Flesher <alexflesher03@yahoo.com>
2024-09-29 09:03:17 -0500

    relinked gameover screen

--- app/static/scripts/game.js
+++ app/static/scripts/game.js
@@ -38,7 +38,7 @@ async function main() {
       gameOver = false
     } else {
       gameOver = true
-      window.location.href="./static/lose.html";
+      window.location.href="../static/templates/lose.html";
     }
   }
 
