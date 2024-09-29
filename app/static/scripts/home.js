9e77a867b2258eb941e46651966436d633b01227
root <root@DESKTOP-NJ5E2AI>
2024-09-29 01:48:29 -0500

    not empty

--- app/static/scripts/home.js
+++ app/static/scripts/home.js
@@ -1,8 +1,10 @@
 // //import { getToken, getAuthHeader, searchForRandomSong } from "../../Spotify-API/api";
-
+//import React from 'react';
 document.addEventListener("DOMContentLoaded", function() {
     const startButton = document.getElementById("startButton"); // Use your button's ID
     startButton.addEventListener("click", function() {
-        window.location.href = "../../templates/game.html"; // Navigate to the game page
+        alert("1")
+        window.location.href=fetch('login')
+        alert(window.location.href)
     });
 });
